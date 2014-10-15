1d6094197400ddc5deec895bb65c4530d2003f8d
unknown-user <unknown-user@192.168.3.46>
2014-10-15 15:33:10 +0530

    Initial check-in of module TestApp15Oct3

---
+++ TestApp15Oct3/TestApp15Oct3ViewController.designer.cs
@@ -0,0 +1,17 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+
+namespace TestApp15Oct3
+{
+	[Register ("TestApp15Oct3ViewController")]
+	partial class TestApp15Oct3ViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
